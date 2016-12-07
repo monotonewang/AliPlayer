2554efc37cc432884d7bb3eed6b4234d14f5aae3
montotone <zgjxfuqingwang@gmail.com>
2016-12-07 17:29:07 +0800

    permission

--- app/src/main/java/com/alivc/testmediaplayer/MainActivity.java
+++ app/src/main/java/com/alivc/testmediaplayer/MainActivity.java
@@ -1,16 +1,38 @@
 package com.alivc.testmediaplayer;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private String TAG="MainActivity";
+    private int REQUEST_CODE_PERMISSION = 0x00099;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] permissions={Manifest.permission_group.PHONE,Manifest.permission_group.STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        boolean b = checkPermissions(permissions);
+        Log.i(TAG, "onCreate: "+b);
+        List<String> deniedPermissions = getDeniedPermissions(permissions);
+        Log.i(TAG, "onCreate: "+deniedPermissions);
+
     }
 
     public void openDemoOne(View view){
@@ -20,4 +42,139 @@ public class MainActivity extends Activity {
     public void openDemoTwo(View view) {
         startActivity(new Intent(this,PlayerTwoActivity.class));
     }
+    /**
+     * 检测所有的权限是否都已授权
+     *
+     * @param permissions
+     * @return
+     */
+    private boolean checkPermissions(String[] permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 获取权限集中需要申请权限的列表
+     *
+     * @param permissions
+     * @return
+     */
+    private List<String> getDeniedPermissions(String[] permissions) {
+        List<String> needRequestPermissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                needRequestPermissionList.add(permission);
+            }
+        }
+        return needRequestPermissionList;
+    }
+    /**
+     * 请求权限
+     *
+     * @param permissions 请求的权限
+     * @param requestCode 请求权限的请求码
+     */
+    public void requestPermission(String[] permissions, int requestCode) {
+        this.REQUEST_CODE_PERMISSION = requestCode;
+        if (checkPermissions(permissions)) {
+            permissionSuccess(REQUEST_CODE_PERMISSION);
+        } else {
+            List<String> needPermissions = getDeniedPermissions(permissions);
+            ActivityCompat.requestPermissions(this, needPermissions.toArray(new String[needPermissions.size()]), REQUEST_CODE_PERMISSION);
+        }
+    }
+
+
+    /**
+     * 系统请求权限回调
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (verifyPermissions(grantResults)) {
+                permissionSuccess(REQUEST_CODE_PERMISSION);
+            } else {
+                permissionFail(REQUEST_CODE_PERMISSION);
+                showTipsDialog();
+            }
+        }
+    }
+
+    /**
+     * 权限获取失败
+     * @param requestCode
+     */
+    public void permissionFail(int requestCode) {
+        Log.d(TAG, "获取权限失败=" + requestCode);
+    }
+
+    /**
+     * 显示提示对话框
+     */
+    private void showTipsDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("提示信息")
+                .setMessage("当前应用缺少必要权限，该功能暂时无法使用。如若需要，请单击【确定】按钮前往设置中心进行权限授权。")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startAppSettings();
+                    }
+                }).show();
+    }
+
+    /**
+     * 启动当前应用设置页面
+     */
+    private void startAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    /**
+     * 确认所有的权限是否都已授权
+     *
+     * @param grantResults
+     * @return
+     */
+    private boolean verifyPermissions(int[] grantResults) {
+        for (int grantResult : grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 获取权限成功
+     *
+     * @param requestCode
+     */
+    public void permissionSuccess(int requestCode) {
+        Log.d(TAG, "获取权限成功=" + requestCode);
+
+    }
+
 }
